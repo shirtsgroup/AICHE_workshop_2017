1b96af2b603c6e9e9dade83b7b6c64b99727d255
Michael Shirts <mrshirts@gmail.com>
2017-10-29 14:42:39 -0500

    A version of the heat capacity script with the answers.

---
+++ heat_capacity/calc_cv_withanswer.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+# number of data points to throw out as unequilibrated 
+nequil = 500
+
+# temperatures we ran at
+temps = [220,230]
+tlow = temps[0]
+thigh = temps[1]
+
+# Boltzmann constant in kJ/mol*K
+kb = 0.008314
+
+data = dict()
+experiments = [1,2]  # two sets of experiments.
+for e in experiments:
+    for t in temps:
+        # load in data for the experiment.
+        filename = 'isobutane_' + str(t) + 'K_' + str(e) + '.xvg'
+        f = open(filename,'r')
+        lines = f.readlines()
+        f.close()
+        evals = list()
+        for l in lines:
+            if l[0] not in ['#','@']:
+                evals.append(np.float(l.split()[1]))
+        data[t] = np.array(evals)
+
+    # now compute heat capacity using fluctuation formula.
+    Cv_fluct = ((np.std(data[tlow][nequil:])/tlow)**2)/kb
+    print("CV through fluctuations {:8.4f}".format(Cv_fluct))
+
+    # can you compute through the difference formula?
+    Cv_diff = (np.mean(data[thigh]) - np.mean(data[tlow]))/(thigh-tlow)
+    print("CV through finite difference {:8.4f}".format(Cv_diff))
+
